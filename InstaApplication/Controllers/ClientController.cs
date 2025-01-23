4916883c58ddf4742d5fb467389ecc4078a47d81
Chadai Mabiala <chadaimabiala@gmail.com>
2025-01-23 20:26:04 +0100

    premier commit

--- InstaApplication/Controllers/ClientController.cs
+++ InstaApplication/Controllers/ClientController.cs
@@ -1,33 +1,158 @@
-﻿using InstaApplication.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using InstaApplication.Models;
 
-namespace InstaApplication.Controllers
+
+namespace AspMVC.Controllers
 {
     public class ClientController : Controller
     {
-        public IActionResult VueClient()
+        private readonly Myctx _context;
+
+        public ClientController(Myctx context)
         {
-            var Client = new Client
+
+            _context = context;
+        }
+
+        //GET: Produits
+        public IActionResult Index()
+        {
+            return View(_context.Clients.ToList());
+        }
+
+        //GET: Produits/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
             {
-                Id = 1,
-                Name = "Hiong",
-                Prenom = "Esther",
-                age = 22,
-                adresse = "17 RUE LINNE 75005"
-            };
+                return NotFound();
+            }
 
-            ViewData["Client"] = Client;
-            return View();
+            var Clients = _context.Clients
+                .FirstOrDefault(m => m.Id == id);
+            if (Clients == null)
+            {
+                return NotFound();
+            }
+
+            return View(Clients);
         }
 
-        public IActionResult Ajouter()
+        // GET: Produits/Create
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
 
-        public IActionResult supprimer()
+
+
+        [HttpPost]
+        public IActionResult Create([Bind("Id,Name,Prenom,age,adresse")] Client Clients)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                _context.Add(Clients);
+                _context.SaveChanges();
+                //return RedirectToAction(nameof(Index));
+            }
+            return View(Clients);
+        }
+
+        // GET: Produits/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Clients = _context.Clients.Find(id);
+            if (Clients == null)
+            {
+                return NotFound();
+            }
+            return View(Clients);
+        }
+
+        // POST: Produits/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Name,Prenom,age,adresse")] Client Clients)
+        {
+            if (id != Clients.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Clients);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientExists(Clients.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Clients);
+        }
+
+        // GET: Produits/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Clients = _context.Clients
+                .FirstOrDefault(m => m.Id == id);
+            if (Clients == null)
+            {
+                return NotFound();
+            }
+
+            return View(Clients);
+        }
+
+        // POST: Produits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var Clients = _context.Clients.Find(id);
+            if (Clients != null)
+            {
+                _context.Clients.Remove(Clients);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClientExists(int id)
+        {
+            return _context.Clients.Any(e => e.Id == id);
         }
     }
 }
---
+++ InstaApplication/Controllers/RedirectController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InstaApplication.Controllers
+{
+    public class RedirectController : Controller
+    {
+        // Action de redirection vers une autre action dans le même contrôleur
+        public IActionResult RedirectToAnotherAction()
+        {
+            // Redirige vers l'action "TargetAction"
+            return RedirectToAction("TargetAction");
+        }
+
+        // Action de redirection vers une autre action dans un autre contrôleur
+        public IActionResult RedirectToDifferentController()
+        {
+            // Redirige vers l'action "Index" dans le contrôleur "Home"
+            return RedirectToAction("Index", "Home");
+        }
+
+        // Action de redirection vers une URL externe
+        public IActionResult RedirectToExternalUrl()
+        {
+            // Redirige vers une URL externe (ex. Google)
+            return Redirect("https://www.google.com");
+        }
+
+        // Action cible pour la redirection
+        public IActionResult TargetAction()
+        {
+            return Content("Vous avez été redirigé vers cette action.");
+        }
+    }
+}
